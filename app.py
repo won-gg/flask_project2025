c91cb97521ec5eaa545cc85e194228dd28353afc
hdsitsmee <dasom06262@naver.com>
2025-11-03 23:01:14 +0900

    database and login and register practice

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database import DBhandler
+import hashlib
 import sys
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
+DB = DBhandler()
 
 @application.route("/")
 def hello():
   return render_template("index.html")
-
+@application.route("/login")
+def login():
+  return render_template("login.html")
+@application.route("/signup")
+def signup():
+  return render_template("signup.html")
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+  data=request.form
+  pw=request.form['pw']
+  pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+  if DB.insert_user(data,pw_hash):
+    return render_template("login.html")
+  else:
+    flash("user id already exist!")
+    return render_template("signup.html")
 @application.route("/list")
 def view_list():
   return render_template("list.html")
@@ -31,18 +50,15 @@ def reg_item_submit():
   phone=request.args.get("phone")
   print(name,seller,addr,email,category,card,status,phone)
   #return render_template("reg_item.html")
-@application.route("/submit_item_post", methods=['POST'])
-def reg_item_submit_post():
-  data=request.form
-  return render_template("submit_item_result.html", data=data)
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
   image_file=request.files["file"]
   image_file.save("static/images/{}".format(image_file.filename))
   data=request.form
-  return render_template("submit_item_result.html", data=data,
-img_path="static/images/{}".format(image_file.filename))
+  DB.insert_item(data['name'], data, image_file.filename)
+  return render_template("submit_item_result.html", data=data, img_path=
+  "static/images/{}".format(image_file.filename))
 
 if __name__ == "__main__":
   application.run(host='0.0.0.0', debug=True)
