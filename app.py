bfe38dca99b36e3dfde6959399743bbb80cb79e8
yoonad16 <ellajya@ewha.ac.kr>
2025-11-19 19:02:08 +0900

    Merged stashed changes for app.py into yoona_jeong

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ def profile():
   return render_template("profile.html")
 
 if __name__ == "__main__":
-  application.run(host='0.0.0.0', debug=True)
+  application.run(host='0.0.0.0', debug=True)g
